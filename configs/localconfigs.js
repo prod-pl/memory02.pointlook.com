f3cc843947b33ecfe62345f9155a900536ffac73
Alex Davis <alex2006hw@gmail.com>
2017-12-28 16:43:02 -0800

    201712.28.1514508178

--- configs/localconfigs.js
+++ configs/localconfigs.js
@@ -4,19 +4,19 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 /////
-var HOSTNAME = exports.HOSTNAME = "memory02.usertoken.com";
+var HOSTNAME = exports.HOSTNAME = "memory02.pointlook.com";
 var DATA_FILE = exports.DATA_FILE = "data/data-" + HOSTNAME + "-client";
 var DEVICE_ID = exports.DEVICE_ID = false;
 
-var MY_MEMORY = exports.MY_MEMORY = CLOUD_MEMORIES;
-var PROD_CLOUD_MEMORIES = exports.PROD_CLOUD_MEMORIES = "https://m2-ut-admin.193b.starter-ca-central-1.openshiftapps.com/gun";
-var CLOUD_MEMORIES = exports.CLOUD_MEMORIES = ["https://dev-ut-redhat.193b.starter-ca-central-1.openshiftapps.com/gun", "https://dev-ut-memtwo.193b.starter-ca-central-1.openshiftapps.com/gun"];
+var MY_MEMORY = exports.MY_MEMORY = "https://m2-pl-admin.193b.starter-ca-central-1.openshiftapps.com/gun";
+var CLOUD_MEMORIES = exports.CLOUD_MEMORIES = MY_MEMORY;
+var DEV_CLOUD_MEMORIES = exports.DEV_CLOUD_MEMORIES = ["https://dev-ut-redhat.193b.starter-ca-central-1.openshiftapps.com/gun", "https://dev-ut-memtwo.193b.starter-ca-central-1.openshiftapps.com/gun"];
 var ROOT_MEMORIES = exports.ROOT_MEMORIES = ["https://tropospheric.mybluemix.net/gun", "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun"];
 //export const MY_MEMORY = "https://memory02.alex2006hw.com/gun";
 //export const ROOT_MEMORIES = [
 //  "https://memory02.alex2006hw.com/gun",
 //  "https://memory02.pointlook.com/gun",
-//  "https://memory02.alex2006hw.com/gun"
+//  "https://memory02.usertoken.com/gun"
 //];
 //export const PEER_MEMORIES = [
 //  "https://tropospheric.mybluemix.net/gun",
